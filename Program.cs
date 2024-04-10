eacb44accac140091d69b10aae6096d4e420aa60
mariia <mariia.ill-maria@ya.ru>
2024-04-10 20:40:56 +0300

    Description of the solution

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+static void Main(string[] args)
+{
+    string[] inputArray = { "Good", "Hello", ",./", "77" };
+    int count = 0;
+    foreach (string str in inputArray)
+    {
+        if (str.Length <= 3)
+            count++;
+    }
+
+    string[] filteredArray = new string[count];
+    int index = 0;
+    foreach (string str in inputArray)
+    {
+        if (str.Length <= 3)
+        {
+            filteredArray[index] = str;
+            index++;
+        }
+    }
+
+    Console.WriteLine("Отфильтрованный массив строк:");
+    foreach (string str in filteredArray)
+    {
+        Console.WriteLine(str);
+    }
+}
+}
